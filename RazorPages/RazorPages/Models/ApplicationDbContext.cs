fa8282c3daa4dc71260c679649ab493e0e179a72
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-12-28 00:10:30 +0100

    Add contact manager DbContext

---
+++ RazorPages/RazorPages/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages.Models
+{
+    /// <summary>
+    /// Create a session with the database for querying and storing instances of entities.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
