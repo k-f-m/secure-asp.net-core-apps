8f314ff065e364dc6002717d6fcf98e7f2edca7d
k-f-m <55965735+k-f-m@users.noreply.github.com>
2023-12-27 23:59:42 +0100

    Add contact model

---
+++ RazorPages/RazorPages/Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPages.Models
+{
+    /// <summary>
+    /// Represents a contact entity.
+    /// </summary>
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; private set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Salutation { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(3)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(3)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
